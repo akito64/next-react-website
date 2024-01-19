d61851f4c8f65448e7fec8daec3bf75a97ff0537
akito64 <reimu6akito4@icloud.com>
2024-01-19 16:28:45 +0900

    2024/01/19
    chapter 9

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,6 +1,16 @@
 import { getPostBySlug } from 'lib/api'
+import { extractText } from 'lib/extract-text'
+import Meta from 'components/meta'
 import Container from 'components/container'
 import PostHeader from 'components/post-header'
+import PostBody from 'components/post-body'
+import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from 'components/two-column'
+import ConvertBody from 'components/convert-body'
+import PostCategories from 'components/post-categories'
+import Image from 'next/image'
+
+// ローカルの代替アイキャッチ画像
+import { eyecatchLocal } from 'lib/constants'
 
 export default function Schedule({
     title,
@@ -8,28 +18,62 @@ export default function Schedule({
     content,
     eyecatch,
     categories,
+    description,
 }) {
     return (
         <Container>
+            <Meta
+                pageTitle={title}
+                pageDesc={description}
+                pageImg={eyecatch.url}
+                pageTmgW={eyecatch.width}
+                pageImgH={eyecatch.height}
+            />
             <article>
                 <PostHeader title={title} subtitle="Blog Article" publish={publish} />
+
+                <figure>
+                    <Image
+                        src={eyecatch.url}
+                        alt=""
+                        layout="responsive"
+                        width={eyecatch.width}
+                        height={eyecatch.height}
+                        sizes="(min-width: 1152px) 1152px, 100vw"
+                        priority
+                    />
+                </figure>
+
+                <TwoColumn>
+                    <TwoColumnMain>
+                        <PostBody>{content}</PostBody>
+                    </TwoColumnMain>
+                    <TwoColumnSidebar>
+                        <PostCategories categories={categories} />
+                    </TwoColumnSidebar>
+                </TwoColumn>
             </article>
         </Container>
     )
 }
 
 export async function getStaticProps() {
-    const slug = 'schedule'
+    const slug = 'micro'
 
     const post = await getPostBySlug(slug)
+
+    const description = extractText(post.content)
+
+    const eyecatch = post.eyecatch ?? eyecatchLocal
 
     return {
         props: {
             title: post, title,
             publish: post.publishDate,
             content: post.content,
-            eyecatch: post.eyecatch,
-            categories:post.categories,
+            eyecatch: eyecatch,
+            categories: post.categories,
+            description: description,
         },
     }
 }
